1ce9d9557fd91c97b4df8f63a509a8bdadf90efe
Severin Kaderli <severin.kaderli@gmail.com>
2016-01-27 21:23:58 +0100

    Added problem 1

---
+++ 001.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Author: Severin Kaderli <severin.kaderli@gmail.com>
+#
+# Project Euler - Problem 1:
+# Find the sum of all the multiples of 3 or 5 below 1000.
+def get_sum(limit = 1000):
+    sum = 0
+    for n in range(1, limit):
+        if n % 3 == 0 or n % 5 == 0:
+            sum += n 
+    return sum
+
+if __name__ == "__main__":     
+    print(get_sum())
