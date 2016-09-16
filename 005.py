0ca9317783e4f479a3e011247ea2b459f6f22151
Severin Kaderli <severin.kaderli@gmail.com>
2016-09-16 14:01:12 +0200

    Add problem 5
    Signed-off-by: Severin Kaderli <severin.kaderli@gmail.com>

---
+++ 005.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Author: Severin Kaderli <severin.kaderli@gmail.com>
+#
+# Project Euler - Problem 5:
+# What is the smallest positive number that is evenly divisible by all of the
+# numbers from 1 to 20?
+from functools import reduce
+
+def gcd(a, b):
+    """Returns the greatest common divisor using Euclid's Algorithm."""
+    while b:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    "Returns the lowest common multiple."
+    return a * b // gcd(a, b)
+
+def lcmm(*args):
+    "Returns the lcm of multiple numbers."
+    return reduce(lcm, args)  
+
+if __name__ == "__main__":
+    print(lcmm(*range(1, 21)))
